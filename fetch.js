266460c5889a8390ab309ed3d23defc15b019c68
edwingc88 <edwingc88@gmail.com>
2020-07-27 19:27:34 +0200

    5 commit

--- fetch.js
+++ fetch.js
@@ -2,12 +2,13 @@ const cheerio= require('cheerio'),
     fetch= require('node-fetch');
 
  async function main(){
-    let response= await fetch("https://www.workana.com/jobs?category=it-programming&subcategory=wordpress-1&language=en%2Ces");
+    let response= await fetch("https://www.workana.com/jobs?category=it-programming&subcategory=mobile-development&language=es");
     let html= await response.text();
 
     let $ = cheerio.load(html);
-    console.log($('.project-item > .project-header > h5 > strong').text().trim());
-    console.log($('.project-item > .project-header > h2 > a').attr());
+    console.log($('.project-header > h5').text());
+   //console.log($('.project-item > .project-header > h5 > strong').text());
+    //console.log($('.project-item > .project-header > h2 > a').attr());
 }   
 
  main();
